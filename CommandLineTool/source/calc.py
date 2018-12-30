573fc0ded8706ad3404fe1c32a2a053d49c6683c
Christian Gröling <ch.groeling@gmail.com>
2018-12-30 15:59:25 +0100

    Added autodoc string and changed docstrings to google format.

--- CommandLineTool/source/calc.py
+++ CommandLineTool/source/calc.py
@@ -1,20 +1,58 @@
 def add(x, y):
-    """Add Function"""
+    """Add Function
+    
+    Arguments:
+        x: First argument
+        y: Second argument
+    
+    Returns:
+        x + y 
+    """
     return x + y
 
 
 def subtract(x, y):
-    """Subtract Function"""
+    """Subtract Function
+    
+    Arguments:
+        x: First argument
+        y: Second argument
+    
+    Returns:
+        x - y 
+    """
     return x - y
 
 
 def multiply(x, y):
-    """Multiply Function"""
+    """Multiply Function
+    
+    Arguments:
+        x: First argument
+        y: Second argument
+    
+    Returns:
+        x * y 
+    """
+
     return x * y
 
 
 def divide(x, y):
-    """Divide Function"""
+    """Divide Function
+    
+    Arguments:
+        x: First argument
+        y: Second argument
+    
+    Raises:
+        ValueError: Is raised when y = 0
+    
+    Returns:
+        x / y
+    """
+
+
     if y == 0:
         raise ValueError('Can not divide by zero!')
     return x / y
