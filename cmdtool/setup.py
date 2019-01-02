3ac23137e4c175a18cd5a4051b339dc4e787508d
Christian Gröling <ch.groeling@gmail.com>
2019-01-02 15:48:51 +0100

    Added a package manager setup script

---
+++ cmdtool/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='cmdtool',
+    version='0.0.1',
+    packages=['cmdtool',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
