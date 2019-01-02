219ffea2510b48f89e599440edd2854f5d3f70e2
Christian Gröling <ch.groeling@gmail.com>
2019-01-02 18:01:11 +0100

    Added entry point

--- cmdtool/setup.py
+++ cmdtool/setup.py
@@ -6,4 +6,11 @@ setup(
     packages=['cmdtool',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
+    
+    # Setuptools allows modules to register entrypoints which other 
+    # packages can hook into to provide certain functionality. 
+    # It also provides a few itself, including the console_scripts entry point.
+    entry_points = {
+        'console_scripts': ['cmdtool=cmdtool.cmdtool:main'],
+    }
 )
