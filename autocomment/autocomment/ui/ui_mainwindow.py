ff6d7e248bf90f980cb9e20ff6dca3afc7fe40e0
Christian Groeling <ch.groeling@gmail.com>
2019-01-10 15:39:53 +0100

    Removed statusbar

--- autocomment/autocomment/ui/ui_mainwindow.py
+++ autocomment/autocomment/ui/ui_mainwindow.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file './autocomment/ui/mainwindow.ui'
+# Form implementation generated from reading ui file 'mainwindow.ui'
 #
-# Created by: PyQt5 UI code generator 5.9.2
+# Created by: PyQt5 UI code generator 5.6
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -29,12 +29,9 @@ class Ui_MainWindow(object):
         self.verticalLayout_2.addWidget(self.exitButton)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 573, 22))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 573, 26))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
