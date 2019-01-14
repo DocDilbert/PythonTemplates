1e57d53003064ee11b38b30dbc7640f336cd5112
Christian Groeling <ch.groeling@gmail.com>
2019-01-14 17:17:18 +0100

    Reduced friction

--- autocomment/autocomment/ui/ui_dialog.py
+++ autocomment/autocomment/ui/ui_dialog.py
@@ -11,7 +11,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(706, 538)
+        Dialog.resize(996, 750)
         self.verticalLayout = QtWidgets.QVBoxLayout(Dialog)
         self.verticalLayout.setObjectName("verticalLayout")
         self.textEdit = QtWidgets.QTextEdit(Dialog)
