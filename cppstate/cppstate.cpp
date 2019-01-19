7ffa6bcbc88c31371392e138cf4fb3ba006060a3
Christian Gröling <ch.groeling@gmail.com>
2019-01-19 10:56:31 +0100

    Generated simple state machine

--- cppstate/cppstate.cpp
+++ cppstate/cppstate.cpp
@@ -1,11 +1,64 @@
-//[[[cog import cog]]]
+//[[[cog 
+//  import cog
+//  import json
+//  with open('config.json') as f:
+//    config = json.load(f)
+//  states = config['states']
+//  states_ids = {state: 'ID_'+state.upper() for state in states}
+//]]]
 //[[[end]]]
 
-//[[[cog
-//  for table in ['customers', 'orders', 'suppliers']:
-//      cog.outl("drop1 table %s;" % table)
+enum StateId
+{
+  //[[[cog cog.outl("\n".join(states_ids.values())) ]]]
+  //[[[end]]]
+}
+
+class IStateMachine
+{
+  public:
+  	virtual void setNextState(StateId state) = 0;
+}
+
+
+class IState 
+{
+  public:
+    virtual State getId() = 0;
+    virtual void update() = 0;
+}
+
+
+//[[[cog 
+//  for s in states:
+//    cog.outl('class '+s+': public IState')
+//    cog.outl('{')
+//    cog.outl('public:')
+//    cog.outl('  {}::{}(IStateMachine& statemachine)'.format(s, s))
+//    cog.outl('  {')
+//    cog.outl('    this->statemachine = statemachine;')
+//    cog.outl('  }')
+//    cog.outl()
+//    cog.outl('  StateId getId()')
+//    cog.outl('  {')
+//    cog.outl('    return '+states_ids[s]+";")
+//    cog.outl('  }')
+//    cog.outl()
+//    cog.outl('  void update()')
+//    cog.outl('  {')
+//    cog.outl('  	// Check for transitions here ...')
+//    cog.outl('  }')
+//    cog.outl()
+//    cog.outl('  void setNextState(StateId state)')
+//    cog.outl('  {')
+//    cog.outl('  	// Insert transition logic here ...')
+//    cog.outl()
+//    cog.outl('  	context.setNexState(state)')
+//    cog.outl('  }')
+//    cog.outl()
+//    cog.outl('private:')
+//    cog.outl('  IStateMachine& statemachine;')
+//    cog.outl('}')
+//    cog.outl()
 //]]]
-drop1 table customers;
-drop1 table orders;
-drop1 table suppliers;
 //[[[end]]]
