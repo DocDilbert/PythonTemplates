e06f86ce238d64850439f26fad58fca8f1242ae3
Christian Groeling <ch.groeling@gmail.com>
2019-01-23 18:34:04 +0100

    State machine generated

--- cppstate/cppstate.cpp
+++ cppstate/cppstate.cpp
@@ -6,6 +6,7 @@
 //    config = json.load(f)
 //  states = config['states']
 //  states_ids = {state: 'ID_'+state.upper() for state in states}
+//  transitions = config['transitions']
 //]]]
 //[[[end]]]
 
@@ -25,15 +26,75 @@ class IStateMachine
 class IState 
 {
   public:
-    virtual State getId() = 0;
+
+    /// This method returns the Id of the state
+    virtual StateId update getId() = 0;
+
+    /// The entry method is called by statemachine the first time this
+    /// state is executed
+    virtual void entry() = 0;
+
+    /// The update method is called every time when the state is 
+    /// active
     virtual void update() = 0;
 }
 
-
 //[[[cog 
-//  for s in states:
-//    state = cppstate.state_class.StateClass(s)
+//  last_state = states[-1]
+//  for state_name in states:
+//    state_transitions = [transition for transition in transitions if transition['from']==state_name]
+//    state = cppstate.state_class.StateClass(state_name, state_transitions)
 //    state.out()
-//    cog.outl()
+//    if state_name != last_state:
+//      cog.outl()
 //]]]
 //[[[end]]]
+
+class StateMachine : public IStateMachine
+{
+public:
+    IState* getIStateFromId(StateId stateId)
+    {
+        //[[[cog 
+        //  last_state = states[-1]
+        //  for state_name in states:
+        //    sid = states_ids[state_name]
+        //    cog.outl("case {}:".format(sid))
+        //    cog.outl("{")
+        //    cog.outl("    return &{};".format(state_name.lower()))
+        //    cog.outl("}")
+        //    if state_name != last_state:
+        //      cog.outl()
+        //]]]
+        //[[[end]]]
+    }
+
+    void update()
+    {
+        if (callEntry)
+        {
+          // only call entry once 
+          istate->entry();
+          callEntry = false;
+        }
+        istate->update();
+    }
+
+    void setNextState(StateId state)
+    {
+      this.istate = getIStateFromId(state);
+      this.callEntry = true; // self transitions also call entry()
+    }
+
+private:
+    StateId state;
+    IState *istate;
+    BOOL callEntry;
+
+    //[[[cog 
+    //  last_state = states[-1]
+    //  for state_name in states:
+    //    cog.outl("{} {};".format(state_name, state_name.lower()))
+    //]]]
+    //[[[end]]]
+}
--- cppstate/cppstate/state_class.py
+++ cppstate/cppstate/state_class.py
@@ -1,9 +1,11 @@
 import cog
 class StateClass:
-    def __init__(self, name):
+    def __init__(self, name, transitions=None):
         self.__name = name
         self.__indent = ""
         self.__indentSpaceCount = 0
+        self.__transitions=transitions
+        cog.msg(str(transitions))
 
     def get_id(self):
         return "ID_"+self.__name.upper()
@@ -11,6 +13,9 @@ class StateClass:
     def out_indent(self, str):
         cog.outl("{}{}".format(self.__indent, str))
    
+    def out_nl(self):
+        self.out_indent("")
+
     def out_private(self):
         self.out_indent("private:")
 
@@ -20,7 +25,13 @@ class StateClass:
     def out_class(self):
         self.out_indent("class {} : public IState".format(self.__name))
 
-     
+    def out_constructor_prototype(self, initializer_list = True):
+        ilist = ""
+        if initializer_list:
+            ilist=":"
+
+        self.out_indent("{}::{}() {}".format(self.__name, self.__name, ilist))
+
     def out_begin(self):
         self.out_indent("{")
 
@@ -36,12 +47,29 @@ class StateClass:
     def out_member(self, *, name, type):
         self.out_indent("{} {};".format(type, name))
 
-    def out_method(self,*, name, arguments="", returns="void"):
+    def out_method_prototype(self,*, name, arguments="", returns="void"):
         self.out_indent("{} {}({})".format(
             returns,
             name, 
             arguments
         ))
+
+    def out_state_check(self, name):
+        self.out_method_prototype(name="check{}".format(name), returns="BOOL")
+        self.out_begin()
+        self.raise_indent()
+        self.out_comment("If transition must be executed return true.")
+        self.out_code("return false")
+        self.lower_indent()
+        self.out_end()
+
+    def out_state_checks(self):
+        if not self.__transitions:
+            return
+
+        for transition in self.__transitions:
+            self.out_state_check(transition['name'])
+            self.out_nl()
 
     def raise_indent(self):
         self.__indentSpaceCount=self.__indentSpaceCount+4
@@ -51,44 +79,97 @@ class StateClass:
         self.__indentSpaceCount=self.__indentSpaceCount-4
         self.__indent=" "*self.__indentSpaceCount   
 
+    def generate_transition_check(self, name, to_state):
+        self.out_indent("if (check{}())".format(name))
+        self.out_begin()
+        self.raise_indent()
+        self.out_code("setNextState({})".format(to_state))
+        self.out_code("return")
+        self.lower_indent()
+        self.out_end()
+
+    def generate_processTransitions(self):
+        self.out_method_prototype(name="processTransitions")
+        self.out_begin()
+        self.raise_indent()
+
+        if self.__transitions:
+            last = self.__transitions[-1]
+            for transition in self.__transitions:
+                self.generate_transition_check(transition['name'], transition['to'])
+
+                if transition!=last:
+                    self.out_nl()
+        else:
+            self.out_comment("Check for transitions here ...")
+        self.lower_indent()
+        self.out_end()
+
     def out(self):
         self.out_class()
         self.out_begin()
         self.out_public()
         self.raise_indent()
+        
+        # Constructor
+        self.out_constructor_prototype(False)
+        self.out_begin()
+        self.raise_indent()
+        self.lower_indent()
+        self.out_end()
+        self.out_nl()
 
-        self.out_method(name="getId", returns="StateId")
+
+        # getId
+        self.out_method_prototype(name="getId", returns="StateId")
         self.out_begin()
         self.raise_indent()
         self.out_code("return "+self.get_id())
         self.lower_indent()
         self.out_end()
-        
-        cog.outl()
+        self.out_nl()
 
-        self.out_method(name="update")
+
+        # check methods
+        self.out_state_checks()
+        self.generate_processTransitions()
+        self.out_nl()
+
+         # entry
+        self.out_method_prototype(name="entry")
         self.out_begin()
         self.raise_indent()
-        self.out_comment("Check for transitions here ...")
+        self.lower_indent()
+        self.out_end()
+        self.out_nl()
+
+
+        # update
+        self.out_method_prototype(name="update")
+        self.out_begin()
+        self.raise_indent()
+        self.out_code("processTransitions()")
+        self.out_nl()
+        self.out_comment("Insert state code here")
         self.lower_indent()
         self.out_end()
 
-        cog.outl()
+        self.out_nl()
 
-        self.out_method(name="setNextState", arguments="StateId state")
+        self.lower_indent()
+        self.out_private()
+        self.raise_indent()
+
+        # setNextState
+        self.out_method_prototype(name="setNextState", arguments="StateId state")
         self.out_begin()
         self.raise_indent()
         self.out_code("stateMachine.setNextState(state)")
         self.lower_indent()
         self.out_end()
 
-        self.lower_indent()
+        self.out_nl()
 
-        cog.outl()
-
-        self.out_private()
-
-        self.raise_indent()
         self.out_member(type="IStateMachine&", name="stateMachine" )
         self.lower_indent()
         self.out_end()
