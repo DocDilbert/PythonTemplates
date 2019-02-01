790b3efb8a47c30628a2c0f497e8d1ace3f3c985
Christian Gröling <ch.groeling@gmail.com>
2019-02-01 09:43:23 +0100

    Added comments

--- git/rewrite_author.sh
+++ git/rewrite_author.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# Changing the author (or committer) would require re-writing all of the history. 
+# If you're okay with that and think it's worth it then you should check out 
+# git filter-branch. The man page includes several examples to get you started. 
+# Also note that you can use environment variables to change the name of the author, 
+# committer, dates, etc. -- see the "Environment Variables" section of the git man page.
+
+# Specifically, you can fix all the wrong author names and emails for all branches 
+# and tags with this command (source: GitHub help):
+
 git filter-branch --env-filter '
 OLD_EMAIL="cgg@festo.net"
 CORRECT_NAME="Christian Gröling"
