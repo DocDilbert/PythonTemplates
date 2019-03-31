3d63264c3e3ee646786a28e69eed11bd40ad3d8c
Christian Gröling <ch.groeling@gmail.com>
2019-03-31 19:33:52 +0200

    added test.py

--- web_scrap/test.py
+++ web_scrap/test.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 
 URL = "https://www.heise.de/newsticker/meldung/Missing-Link-Weimar-1919-Meine-Herren-und-Damen-4356582.html"
 page = requests.get(URL)
+
 with open("index.html","wb") as file:
     file.write(page.content)
     soup = BeautifulSoup(page.content, 'html.parser')
