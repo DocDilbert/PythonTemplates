3fd210b2de17a6bcb0f6b004774954c665afe66f
Christian Gröling <ch.groeling@gmail.com>
2019-03-31 20:18:13 +0200

    Added random numbers into database

---
+++ sqlite3/sqlite3_test.py
@@ -0,0 +1,16 @@
+# 
+import sqlite3 as lite
+import random 
+
+con = lite.connect('./database/test.db')
+
+with con:
+
+    cur = con.cursor()     
+
+    cur.execute("CREATE TABLE IF NOT EXISTS RandomNumbers(randn integer)")
+    sql = "INSERT INTO RandomNumbers VALUES (?)"
+    print(sql)
+    cur.execute(sql, (random.randint(0, 5), ))
+
+
