8cc5b211ed480896925c7cab0479a2484734de46
Christian Gröling <ch.groeling@gmail.com>
2019-04-18 09:09:14 +0200

    Refactoring

--- web_scrap/sqlliteblob.py
+++ web_scrap/sqlliteblob.py
@@ -8,7 +8,7 @@ import os
 import logging
 from urllib.parse import urlparse, urlunparse
 
-from webscrapper_classes import ResponseContent
+from webscrapper_classes import Response, ResponseContent
 
 module_logger = logging.getLogger('main.sqliteblob')
 
@@ -50,7 +50,7 @@ def create_or_open_db(db_file):
         sql = ("CREATE TABLE IF NOT EXISTS RESPONSES ("
                     "ID INTEGER PRIMARY KEY AUTOINCREMENT,"
                     "CONTENT_TYPE TEXT,"
-                    "CONTENT_ID BLOB);")
+                    "CONTENT_ID INTEGER);")
 
         module_logger.debug("conn.execute(%s)", sql)
         conn.execute(sql)
@@ -82,14 +82,14 @@ def insert_response_content(cursor, response_content):
     
     return rid
 
-def insert_response(cursor, content_type, content_id):
+def insert_response(cursor, response, content_id):
     sql =("INSERT INTO RESPONSES ("
             "CONTENT_TYPE,"
             "CONTENT_ID"
             ") VALUES (?, ?);")
 
     cursor.execute(sql, [
-        content_type,
+        response.content_type,
         content_id
     ])
     
@@ -116,25 +116,30 @@ def insert_request_and_response(cursor, timestamp, request, content_type, conten
     """
     
     url = request['url']
-    last_response = extract_last_response_of_request(cursor, url)
+    last_response, last_content_id = extract_last_response_of_request(cursor, url)
     
     if not last_response:
         module_logger.debug("This is the first time this url was requested.")
+
         response_content = ResponseContent(content)
         content_id = insert_response_content(cursor, response_content)
-        response_id = insert_response(cursor, content_type, content_id)
+
+        response = Response(content_type)
+        response_id = insert_response(cursor, response, content_id)
     else:
-        content_id = last_response['content_id']
-        stored_response_content = extract_response_content_by_id(cursor, content_id)
+        stored_response_content = extract_response_content_by_id(cursor, last_content_id)
 
         if (content != stored_response_content.content):
             module_logger.debug("The received response content is new.")
+
             response_content = ResponseContent(content)
             content_id = insert_response_content(cursor, response_content)
         else:
             module_logger.debug("The received response content was stored beforehand. Using this instead.")
+            content_id = last_content_id
 
-        response_id = insert_response(cursor, content_type, content_id)
+        response = Response(content_type)
+        response_id = insert_response(cursor, response, content_id)
         
     sql = ("INSERT INTO REQUESTS ("
                 "TIMESTAMP,"
@@ -205,6 +210,16 @@ def list_all_requests_for_url(cursor, url):
     return data
 
 def extract_response_content_by_id(cursor, rid):
+    """ Extrahiert das unter der rid in der Tabelle RESPONSE_CONTENT 
+    abgelegten ResponseContent Objekt.
+
+    Arguments:
+        cursor -- Datenbank Cursor
+        id -- Die id des gewünschten response content
+
+    Returns:
+        Ein befülltes ResponseContent Objekt.
+    """
     sql = ("SELECT("
                 "CONTENT"
             ")FROM RESPONSE_CONTENT WHERE id = :rid")
@@ -219,31 +234,34 @@ def extract_response_content_by_id(cursor, rid):
 
     return response_content
 
-def extract_response_by_id(cursor, id):
-    """ Extrahiert die unter der id in der Datenbank abgelegte response.
+def extract_response_by_id(cursor, rid):
+    """ Extrahiert das unter der rid in der Tabelle RESPONSES 
+    abgelegte Response Objekt sowie die zugehöre content id.
 
     Arguments:
         cursor -- Datenbank Cursor
-        id -- Die id der gewünschten response
+        rid -- Die id der gewünschten response
 
     Returns:
-        Die response die unter id gespeichert wurde.
+        Ein Tuple bestehend aus der gefundenen Response und der zu
+        dieser Response zugehörigen content_id.
     """
 
-    module_logger.debug("Extract response with id = %i from RESPONSES.", id)
+    
     sql = ("SELECT "
                 "CONTENT_TYPE,"
                 "CONTENT_ID "
-            "FROM RESPONSES WHERE id = :id")
-    param = {'id': id}
+            "FROM RESPONSES WHERE id = :rid")
+    param = {'rid': rid}
     cursor.execute(sql, param)
     x = cursor.fetchone()
+    response = Response(content_type=x[0])
+    content_id =  x[1]
+    module_logger.debug(
+        "Extracted %s with id = %i from RESPONSES. "
+        "Corresponding content_id is %i.", str(response), rid, content_id) 
 
-    return {
-        'id' : id,
-        'content_type' : x[0],
-        'content_id' : x[1]
-    }
+    return (response, content_id)
 
 
 def extract_last_response_of_request(cursor, url):
@@ -254,14 +272,15 @@ def extract_last_response_of_request(cursor, url):
         url -- Die url unter dem die response gesucht werden soll
 
     Returns:
-        Die letzte unter url gespeicherte response.
-        Falls keine response gefunden wird, wird None zurückgegeben.
+        Es wird ein tuple zurückgegeben. Das erste Element ist die
+        gefundene response das zweite die content_id der gefundenen
+        response. Wird kein Element gefunden wird (None, -1) zurückgegeben
     """
     dataset = list_all_requests_for_url(cursor, url)
 
     if len(dataset) == 0:
         module_logger.debug('Found no request in REQUESTS with url "%s"', url)
-        return None
+        return (None, -1)
     else:
         module_logger.debug('Found request(s) in REQUESTS with url "%s"', url)
         module_logger.debug("The last request in this list has the response_id=%i.", dataset[-1]['response_id'])
--- web_scrap/webscrapper_classes.py
+++ web_scrap/webscrapper_classes.py
@@ -1,6 +1,16 @@
 
 BLOB_STR_LENGTH = 10
 
+class Response:
+    def __init__(self, content_type):
+        self.content_type=content_type
+    
+    def __str__(self): 
+        return "{{content_type=\"{}\"}}".format(str(self.content_type))
+
+    def __repr__(self):
+        return "{{content_type=\"{}\"}}".format(str(self.content_type))
+            
 class ResponseContent:
     def __init__(self, content):
         self.content=content
@@ -12,3 +22,4 @@ class ResponseContent:
     def __repr__(self):
         l = min(len(self.content),BLOB_STR_LENGTH)
         return "{{content=\"{}..\"}}".format(str(self.content[0:l]))
+
