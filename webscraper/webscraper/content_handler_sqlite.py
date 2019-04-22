27afe939971e4dafc63d31e281a35514f2432bd5
Christian Gröling <ch.groeling@gmail.com>
2019-04-22 06:44:46 +0200

    Refactoring

--- webscraper/webscraper/content_handler_sqlite.py
+++ webscraper/webscraper/content_handler_sqlite.py
@@ -6,16 +6,17 @@ from webtypes.session import Session
 from webscraper.content_handler_decorator import ContentHandlerDecorator
 import sqliteblob.sqliteblob as sqliteblob
 
-class ContentHandlerSqlite(ContentHandlerDecorator): 
+
+class ContentHandlerSqlite(ContentHandlerDecorator):
     def __init__(self, filename):
         super().__init__()
-        self.connection =  sqliteblob.create_or_open_db(filename)
+        self.connection = sqliteblob.create_or_open_db(filename)
         self.cursor = self.connection.cursor()
-        self.logger = logging.getLogger('webscraper.content_handler_sqllite.ContentHandlerSqlite')
+        self.logger = logging.getLogger(
+            'webscraper.content_handler_sqllite.ContentHandlerSqlite')
 
         self.session_id = -1
         self.session = None
-
 
     def session_started(self):
         super().session_started()
@@ -23,27 +24,30 @@ class ContentHandlerSqlite(ContentHandlerDecorator):
         self.session.update_start_datetime()
         self.session_id = sqliteblob.insert_session(self.cursor, self.session)
 
-    def insert_request_and_response(self,  request, response, RESPONSE_CONTENTS):        
+    def insert_request_and_response(self,  request, response, response_contents):
         self.logger.debug("Insert request and response into database")
 
-        sqliteblob.insert_request_and_response(self.cursor,
+        sqliteblob.insert_request_and_response(
+            self.cursor,
             self.session_id,
             request,
             response,
-            RESPONSE_CONTENTS
+            response_contents
         )
 
-    def response_with_html_content_received(self,  request, response, RESPONSE_CONTENTS):
-        super().response_with_html_content_received( request, response, RESPONSE_CONTENTS)
-        self.insert_request_and_response(request, response, RESPONSE_CONTENTS)
-        
-    def response_with_css_content_received(self,  request, response, RESPONSE_CONTENTS, tag):
-        super().response_with_css_content_received(  request, response, RESPONSE_CONTENTS, tag)
-        self.insert_request_and_response(request, response, RESPONSE_CONTENTS)
-        
-    def response_with_img_content_received(self,  request, response, RESPONSE_CONTENTS, tag):
-        super().response_with_img_content_received(  request, response, RESPONSE_CONTENTS, tag)
-        self.insert_request_and_response( request, response, RESPONSE_CONTENTS)
+    def response_with_html_content_received(self,  request, response, response_content):
+        super().response_with_html_content_received(request, response, response_content)
+        self.insert_request_and_response(request, response, response_content)
+
+    def response_with_css_content_received(self,  request, response, response_content, tag):
+        super().response_with_css_content_received(
+            request, response, response_content, tag)
+        self.insert_request_and_response(request, response, response_content)
+
+    def response_with_img_content_received(self,  request, response, response_content, tag):
+        super().response_with_img_content_received(
+            request, response, response_content, tag)
+        self.insert_request_and_response(request, response, response_content)
 
     def html_post_process_handler(self, request, soup):
         super().html_post_process_handler(request, soup)
@@ -54,10 +58,10 @@ class ContentHandlerSqlite(ContentHandlerDecorator):
         self.session.update_end_datetime()
 
         sqliteblob.update_session(
-            self.cursor, 
-            self.session_id, 
+            self.cursor,
+            self.session_id,
             self.session
         )
-        
+
         # erst am Ende committen
-        self.connection.commit() 
+        self.connection.commit()
