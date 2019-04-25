15aace75338bbe3aafcc50b1e9e24173e58a2e05
Christian Gröling <ch.groeling@gmail.com>
2019-04-25 08:20:03 +0200

    New features

--- webscraper/gas/__main__.py
+++ webscraper/gas/__main__.py
@@ -54,9 +54,15 @@ def main():
     cursor = connection.cursor()
 
     regex = re.compile("/tankstelle/")
+    p1 = 0
+    p2 = 0
     for session, meta in webdb.interface.get_sessions(cursor):
         session_id = meta['session_id']
+
+        
         print("Parsing session {} / {} --> {}".format(session_id, session.start_datetime, session.end_datetime))
+        
+        p1_start = time.time()
         requests = webdb.filters.get_requests_where_session_id_and_content_type(cursor, session_id, CONTENT_TYPE)
 
         requests_filtered = [request for request,_ in requests if regex.match(request.path)]
@@ -65,17 +71,27 @@ def main():
             webdb.filters.get_response_where_session_id_and_request(cursor, session_id, request) 
             for request in requests_filtered
         ]
+        p1_end = time.time()
+
+        p1 = p1 + p1_end - p1_start
+        p2_start = time.time()
         for response,_ in responses:
             parse_response(session_id, response)
 
-        #
+        p2_end = time.time()
+
+        p2 = p2 + p2_end - p2_start
 
         
     
     end = time.time()
+    max_sessions = session_id
     print()
-    print("Execution time {:.3f} seconds.".format(end - start))
-    print("Execution time per session {:.3f} seconds.".format((end - start)/session_id))
+    print("Execution time {:.3f} s.".format(end - start))
+    print("Execution time per session {:.3f} s.".format((end - start)/max_sessions))
+    print("----")
+    print("avg(p1) = {:.3f} s (database access time)".format(p1/max_sessions))
+    print("avg(p2) = {:.3f} s (parsing time)".format(p2/max_sessions))
 
 if __name__ == "__main__":
     main()
