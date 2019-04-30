7a64049d32cc02be6d86ce455c405e081041e2cc
Christian Gröling <ch.groeling@gmail.com>
2019-04-30 10:10:52 +0200

    Add single parse option

--- webscraper/README.txt
+++ webscraper/README.txt
@@ -1,1 +1,9 @@
 Ein Web Scraper mit Datenbank Anbindung.
+
+
+Profiling geht über:
+
+    Cumulative time:
+    python -m cProfile -s cumulative webparser/__main__.py parse > profile.txt
+
+    Total time:
--- webscraper/webparser/__main__.py
+++ webscraper/webparser/__main__.py
@@ -14,6 +14,9 @@ module_logger = logging.getLogger('webparser')
 
 CONTENT_TYPE="text/html; charset=UTF-8"
 
+class SessionIdUnknown(Exception):
+    pass
+
 class FileWriter:
     def __init__(self, mode):
         self.f = open("out.csv", mode,  encoding="utf-8")
@@ -39,12 +42,26 @@ class DummyWriter:
     def add_entry(self, session_id, uuid, headline, adress, products ):
         pass
 
-def parse_response(session_id, response, add_entry):
-    p3 = time.time()
-    soup = BeautifulSoup(response.content.decode("utf-8") ,'lxml')
-    p3 = time.time() - p3
+class ConsoleWriter:
+    def __init__(self):
+        pass
 
-    p4 =  time.time()
+    def add_entry(self, session_id, uuid, headline, adress, products ):
+        for zahl, product,wann1, wann2 in products:
+            print('{};"{}";"{}";"{}";"{}";"{}";"{}";"{}"\n'.format(
+                    session_id,
+                    uuid, 
+                    headline, 
+                    "#?#".join(adress),
+                    zahl, 
+                    product,
+                    wann1,
+                    wann2
+                ))
+
+def parse_response(session_id, response, add_entry):
+    soup = BeautifulSoup(response.content.decode("utf-8") ,'lxml')
+
     div_with_data = soup.find("div", {"data-tankstelle": True})
     uuid = div_with_data["data-tankstelle"]
 
@@ -68,8 +85,10 @@ def parse_response(session_id, response, add_entry):
         product = preisc.find("strong").text
         
         span_with_title=preisc.find("span", {"title":True})
+
         if not span_with_title:
             continue
+
         wann1 = str.strip(str(span_with_title.next_sibling))
         wann2 = span_with_title['title']
         
@@ -81,37 +100,29 @@ def parse_response(session_id, response, add_entry):
         ])
         
     add_entry(session_id, uuid, headline, adress, products)
-    p4 = time.time() - p4
-    return p3, p4
 
 def parse_session(cursor, session_id, session, regex, file_writer):
 
     print("Parsing session {} / {} --> {}".format(session_id, session.start_datetime, session.end_datetime))
-        
-    p1_start = time.time()
+
     requests = webdb.filters.get_requests_where_session_id_and_content_type(cursor, session_id, CONTENT_TYPE)
 
-    requests_filtered = [request for request,_ in requests if regex.match(request.path)]
+    requests_filtered = (request for request,_ in requests if regex.match(request.path))
     
-    responses = [
+    responses = (
         webdb.filters.get_response_where_session_id_and_request(cursor, session_id, request) 
         for request in requests_filtered
-    ]
-    p1 = time.time() - p1_start
-    
-    p2_start = time.time()
-    p2_0 = 0
-    p2_1 = 0
+    )
+
     for response,_ in responses:
-        p2_0_, p2_1_ = parse_response(session_id, response, add_entry=file_writer.add_entry)
-        p2_0 = p2_0 + p2_0_
-        p2_1 = p2_1 + p2_1_
+        parse_response(session_id, response, add_entry=file_writer.add_entry)
 
-    p2 = time.time() - p2_start
-
-    return p1, p2, p2_0, p2_1
-def parse():
+def parse_session_list(cursor, session_list, regex, file_writer):
+    for session, meta in session_list:
+        session_id = meta['session_id']
+        parse_session(cursor, session_id, session, regex, file_writer)
     
+def parse():
     start = time.time()
 
     connection = webdb.db.open_db_readonly("webscraper.db")
@@ -119,33 +130,16 @@ def parse():
 
     regex = re.compile("/tankstelle/")
 
-    p1 = 0
-    p2 = 0
-    p2_0 = 0
-    p2_1 = 0
-
     file_writer = FileWriter("w")
-    for session, meta in webdb.interface.get_sessions(cursor):
-        session_id = meta['session_id']
-        p1_, p2_, p2_0_, p2_1_ = parse_session(cursor, session_id, session, regex, file_writer)
-        p1 += p1_
-        p2 += p2_
-        p2_0 += p2_0_
-        p2_1 += p2_1_
+    session_list = webdb.interface.get_sessions(cursor)
+    parse_session_list(cursor, session_list, regex, file_writer)
         
     end = time.time()
-    max_sessions = session_id
+    max_sessions = session_list[-1][1]['session_id']
     
     print()
     print("Execution time {:.3f} s.".format(end - start))
     print("Execution time per session {:.3f} s.".format((end - start)/max_sessions))
-    print("----")
-    print("  avg(p1) = {:.3f} s (database access time)".format(p1/max_sessions))
-    print("  avg(p2) = {:.3f} s (parsing time)".format(p2/max_sessions))
-    print("----")
-    print("avg(p2_0) = {:.3f} s (creation time)".format(p2_0/max_sessions))
-    print("avg(p2_1) = {:.3f} s (searching time)".format(p2_1/max_sessions))
-    print("----")
 
 def parse_append():
     with open("out.csv", "r") as fp:
@@ -164,15 +158,43 @@ def parse_append():
     for session, meta in sessions:
         print(meta)
 
+def parse_single(session_id):
+    start = time.time()
+    connection = webdb.db.open_db_readonly("webscraper.db")
+    cursor = connection.cursor()
+
+    session_list = [
+        (session, meta) for session, meta in webdb.interface.get_sessions(cursor) 
+        if meta['session_id']==session_id
+    ]
+
+    if len(session_list) != 1:
+        raise SessionIdUnknown()
+
+    regex = re.compile("/tankstelle/")
+
+    file_writer = ConsoleWriter()
+    parse_session_list(cursor, session_list, regex, file_writer)
+    end = time.time()
+
+    max_sessions = session_list[-1][1]['session_id']
+    
+    print()
+    print("----")
+    print("Execution time {:.3f} s.".format(end - start))
+    print("Execution time per session {:.3f} s.".format((end - start)/max_sessions))
+    print("----")
+
 class WebParserCommandLineParser:
     def __init__(self):
         parser = argparse.ArgumentParser(
             prog="webparser",
             description='',
-            usage=("webscraper <command> [<args]\n"
+            usage=("webparser <command> [<args]\n"
                    "\n"
                    "The following commands are supported:\n"
-                   "   parse    parses the database")
+                   "   parse    parses the database\n"
+                   "   single   parses a single session from the database\n")
         )
 
         parser.add_argument(
@@ -200,6 +222,21 @@ class WebParserCommandLineParser:
     
     def append(self):
         parse_append()
+
+    def single(self):
+        parser = argparse.ArgumentParser(
+            prog="webparser single",
+            description='Parses a single session'
+        )
+
+        # prefixing the argument with -- means it's optional
+        parser.add_argument('session_id', type=int)
+        parser.add_argument('--outfile', type=str)
+
+        # now that we're inside a subcommand, ignore the first
+        # TWO argvs, ie the command (git) and the subcommand (commit)
+        args = parser.parse_args(sys.argv[2:])
+        parse_single(args.session_id)
 
     
 def handle_exception(exc_type, exc_value, exc_traceback):
