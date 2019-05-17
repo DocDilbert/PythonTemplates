9a0e2b7681b916135d969b119131c19191e54a32
Christian Gröling <ch.groeling@gmail.com>
2019-05-17 09:01:45 +0200

    Refactoring

--- webscraper/webscraper/request_factories.py
+++ webscraper/webscraper/request_factories.py
@@ -13,6 +13,7 @@ HEADERS = {
                   'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
 }
 
+
 class RequestInternetFcn:
     def __init__(self):
         pass
@@ -20,10 +21,9 @@ class RequestInternetFcn:
     def __call__(self, request):
 
         response_raw = requests.get(
-            request.get_url(), 
+            request.get_url(),
             headers=HEADERS
         )
-        
 
         response = Response.fromGMT(
             status_code=response_raw.status_code,
@@ -33,19 +33,21 @@ class RequestInternetFcn:
         )
 
         return response
-class RequestToInternet:
 
+
+class RequestToInternet:
     def __init__(self):
         pass
 
     def get(self):
         return RequestInternetFcn()
 
+
 class RequestDatabaseFcn:
-    
+
     def __init__(self,  session_id):
         self.session_id = session_id
-    
+
     def __call__(self, request):
 
         response, _ = webdb.filters.get_response_where_session_id_and_request(
@@ -55,16 +57,15 @@ class RequestDatabaseFcn:
         )
         return response
 
+
 class RequestToDatabase:
     def __init__(self, db_name, session_id):
         self.session_id = session_id
         self.db_name = db_name
-       
 
-    def get(self):    
+    def get(self):
         # This is used for spawning multiple processes
-        global __CURSOR__   
-        __CURSOR__  = webdb.db.open_db_readonly(self.db_name).cursor()
-        
-        return RequestDatabaseFcn(self.session_id)
+        global __CURSOR__
+        __CURSOR__ = webdb.db.open_db_readonly(self.db_name).cursor()
 
+        return RequestDatabaseFcn(self.session_id)
--- webscraper/webtypes/response.py
+++ webscraper/webtypes/response.py
@@ -1,15 +1,15 @@
-from  datetime import datetime, timedelta
+from datetime import datetime, timedelta
 
 BLOB_STR_LENGTH = 10
 
 
 class Response:
     def __init__(self, status_code, date, content_type, content):
-        self.content_type=content_type
-        self.status_code=status_code
+        self.content_type = content_type
+        self.status_code = status_code
         self.date = date
         self.content = content
-    
+
     @classmethod
     def fromGMT(cls, status_code, date_gmt, content_type, content):
         dt = datetime.strptime(date_gmt, "%a, %d %b %Y %H:%M:%S %Z")
@@ -19,12 +19,12 @@ class Response:
 
         return cls(status_code, dt, content_type, content)
 
-    def __str__(self): 
+    def __str__(self):
 
-        l = min(len(self.content),BLOB_STR_LENGTH)
+        l = min(len(self.content), BLOB_STR_LENGTH)
 
         return "{{status_code:{}, date:\"{}\", content_type:\"{}\", content:\"{} ...\"}}".format(
-            self.status_code, 
+            self.status_code,
             self.date,
             self.content_type,
             str(self.content[0:l])
