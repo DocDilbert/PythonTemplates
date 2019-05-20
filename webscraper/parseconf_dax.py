b9d1211e8ef930babc717a6c84d67898b9e8e41d
Christian Gröling <ch.groeling@gmail.com>
2019-05-20 09:50:39 +0200

    Added overview

--- webscraper/parseconf_dax.py
+++ webscraper/parseconf_dax.py
@@ -10,10 +10,21 @@ class ResponseParser:
         self.regex = re.compile(r"-(\d+)-inline\.")
         self.add_entry = add_entry
 
-    def parse(self, session_id, request, response):
+    def parse_overview(self, session_id, request, response):
         soup = BeautifulSoup(response.content.decode("utf-8"), 'lxml')
 
+        # get index from url
+        index =  int(request.get_url().split("?")[1].split("=")[1])
         features_dict = {
-            "url" : request.get_url()
+            "type" : "einzelkurs_uebersicht",
+            "index" : index
         }    
+
         self.add_entry(session_id, features_dict)
+
+    def parse(self, session_id, request, response):
+        url = request.get_url()
+
+        if "kurse_einzelkurs_uebersicht" in url:
+            self.parse_overview(session_id, request, response)
+        
