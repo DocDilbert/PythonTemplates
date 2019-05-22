4fe662d82639b5abeb132775eccdf8305d1735d6
Christian Gröling <ch.groeling@gmail.com>
2019-05-22 12:50:07 +0200

    Refactoring.

--- webscraper/stocks.py
+++ webscraper/stocks.py
@@ -6,6 +6,8 @@ SESSION_ID = 1
 def main():
     with open("data_dax/boerse_raw.json", encoding="utf-8") as fp:
         raw_data = json.load(fp)
+ 
+    #pprint.pprint(historie)
 
     uebersicht = [
         {k:v for k, v in x.items() if k != "type"} 
@@ -23,7 +25,7 @@ def main():
         x['branche'] for x in aktien_uebersicht
     })
     pprint.pprint(branchen)
-    branche = "Dienstlstg."
+    branche = "Telekomm."
     aktien_uebersicht_per_branche = [
         {k:v for k, v in x.items() if k != "gattung"} 
         for x in uebersicht
@@ -31,12 +33,13 @@ def main():
     ]
 
     isin = [x['isin'] for x in aktien_uebersicht_per_branche]
+    aktueller_kurs = [x['kurse']['aktueller_kurs'] for x in aktien_uebersicht_per_branche]
     tageshoch = [x['kurse']['tageshoch'] for x in aktien_uebersicht_per_branche]
     tagestief = [x['kurse']['tagestief'] for x in aktien_uebersicht_per_branche]
     wochenhoch = [x['kurse']['wochenhoch'] for x in aktien_uebersicht_per_branche]
     wochentief = [x['kurse']['wochentief'] for x in aktien_uebersicht_per_branche]
 
-    tageshoch, tagestief, wochentief, wochenhoch, isin = zip(*sorted(zip(tageshoch, tagestief,wochentief,wochenhoch, isin)))
+    aktueller_kurs, tageshoch, tagestief, wochentief, wochenhoch, isin = zip(*sorted(zip(aktueller_kurs, tageshoch, tagestief,wochentief,wochenhoch, isin)))
 
     width = 0.5       # the width of the bars: can also be len(x) sequence
     plt.rcdefaults()    
@@ -51,14 +54,16 @@ def main():
     x_pos = np.arange(len(isin))
     
     ax.bar(x_pos, [wh-wt for wh, wt in zip(wochenhoch, wochentief)], width, bottom=wochentief)
-    ax.bar(x_pos, [th-tt for th, tt in zip(tageshoch, tagestief)], 0.75, bottom=tagestief)
+    ax.bar(x_pos, [th-tt for th, tt in zip(tageshoch, tagestief)], width*1.5, bottom=tagestief)
     ax.set_xticks(x_pos)
     ax.set_xticklabels(isin, rotation='vertical')
+
+
     #ax.boxplot([(x,y,z)
     #    for x,y,z in zip(x_pos, wochenhoch, wochentief)
 #
     #])
-    ax.set_yscale("log", nonposy='clip')
+    #ax.set_yscale("log", nonposy='clip')
     plt.title(branche)
     plt.show()
 
