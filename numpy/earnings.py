83dc7a96520633b34a8edb9182c9f4b377132eec
Christian Gröling <ch.groeling@gmail.com>
2019-06-28 15:46:31 +0200

    added

---
+++ numpy/earnings.py
@@ -0,0 +1,9 @@
+salary = 5413
+vacation_money = 2946 / 5313 * salary
+xmas_money = 2946 / 5313 * salary
+tzug = 0.27 * salary
+
+epm = (salary*12 + vacation_money + xmas_money+tzug)/12
+
+print(tzug)
+print(epm)
