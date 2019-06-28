c9ee2f35c944ce5f563cf744a2d0f7cd888b6eac
Christian Gröling <ch.groeling@gmail.com>
2019-06-28 16:14:41 +0200

    add

--- numpy/earnings.py
+++ numpy/earnings.py
@@ -1,9 +1,15 @@
 salary = 5413
-vacation_money = 2946 / 5313 * salary
-xmas_money = 2946 / 5313 * salary
+vacation_yield = 2946 / 5313
+vacation_money = vacation_yield * salary
+
+xmas_yield = 2946 / 5313
+xmas_money = xmas_yield * salary
 tzug = 0.27 * salary
 
-epm = (salary*12 + vacation_money + xmas_money+tzug)/12
+epm = (salary*12 + vacation_money + xmas_money + tzug)/12
 
-print(tzug)
-print(epm)
+print("Salary: {:f}".format(salary))
+print("Factor:  {:f}".format(salary/epm))
+print("Effective salary: {:f}".format(epm))
+print("Difference: {:f}".format(epm-salary))
+print("Emergency per Month: {:f}".format((epm-salary)*0.6))
