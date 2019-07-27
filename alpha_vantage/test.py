799c641a256f13cd72eb0e87604d38fcb4bcc598
Christian Gröling <ch.groeling@gmail.com>
2019-07-27 15:46:33 +0200

    Added test and finplay

---
+++ alpha_vantage/test.py
@@ -0,0 +1,10 @@
+import pprint
+from alpha_vantage.timeseries import TimeSeries
+ts = TimeSeries(key='CZ5LHN6CF6G8JFD8')
+# Get json object with the intraday data and another with  the call's metadata
+data, meta_data = ts.get_daily_adjusted('BAYN')
+
+with open('test.txt','w') as fp:
+    fp.write(str(pprint.pformat(data)))
+    fp.write("\n")
+    fp.write(str(pprint.pformat(meta_data)))
